d3ffa784db08422e14a5f5346740828fbb8ffa38
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2024-03-11 11:43:39 +0100

    upd

---
+++ ApiBasicAuthentication/Authentications/Basic/BasicAuthenticationHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+
+namespace ApiBasicAuthentication.Authentications.Basic
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
